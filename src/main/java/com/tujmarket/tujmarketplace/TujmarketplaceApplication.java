c5b0ea74d3cb8c2a3634fc451de80a2e9dd07e38
ahenry1296 <ahenry1296@gmail.com>
2025-10-18 14:01:07 +0900

    commit

--- src/main/java/com/tujmarket/tujmarketplace/TujmarketplaceApplication.java
+++ src/main/java/com/tujmarket/tujmarketplace/TujmarketplaceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TujmarketplaceApplication {
+public class TujmarketplaceApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(TujmarketplaceApplication.class, args);
 	}
 
