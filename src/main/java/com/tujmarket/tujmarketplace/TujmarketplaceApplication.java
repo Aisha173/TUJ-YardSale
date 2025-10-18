3fb0989cf91c3240326dd125c4ce356a67332c8a
ahenry1296 <ahenry1296@gmail.com>
2025-10-18 13:50:35 +0900

    test

--- src/main/java/com/tujmarket/tujmarketplace/TujmarketplaceApplication.java
+++ src/main/java/com/tujmarket/tujmarketplace/TujmarketplaceApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TujmarketplaceApplication {
 
 	public static void main(String[] args) {
-		//Git test
-		//kamaBranch Test
-		//branch
 		SpringApplication.run(TujmarketplaceApplication.class, args);
 	}
 
