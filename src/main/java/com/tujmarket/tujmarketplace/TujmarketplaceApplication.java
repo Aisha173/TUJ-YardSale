39948eb16d3f98611fb165742aa970b3c3e5b3f4
ahenry1296 <ahenry1296@gmail.com>
2025-10-18 12:39:14 +0900

    Initial commit

---
+++ src/main/java/com/tujmarket/tujmarketplace/TujmarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.tujmarket.tujmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TujmarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TujmarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tujmarket/tujmarketplace/TujmarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tujmarket.tujmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TujmarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
